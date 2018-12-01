595b5d0f97bae7318303acd594670b6d30f2cb1f
guoliang <guoliang0511@qq.com>
2018-12-01 13:01:56 +0800

    167. Two Sum II - Input array is sorted

---
+++ 0167/solution0617.go
@@ -0,0 +1,34 @@
+package _167
+
+// 167. Two Sum II - Input array is sorted
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+/*
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+Example:
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+ */
+
+func twoSum(numbers []int, target int) []int {
+	i, j := 0, len(numbers)-1
+	for i < j {
+		sum := numbers[i] + numbers[j]
+		if sum == target {
+			return []int{i + 1, j + 1}
+		} else if sum < target {
+			i++
+		} else {
+			j--
+		}
+	}
+	return nil
+}
+
+// Runtime: 4 ms, faster than 100.00% of Go online submissions for Two Sum II - Input array is sorted.
